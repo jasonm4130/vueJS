05edb4f925cd6baa85793e66fcfc94ca70d0e72c
Jason <jason@pop-os.localdomain>
2019-05-05 12:50:51 +1000

    working on assessment 2

---
+++ Assignment 2/app.js
@@ -0,0 +1,82 @@
+window.onload = function() {
+    new Vue({
+        el: '#app',
+        data: {
+            gameRunning: false,
+            turns: [],
+            player: {
+                health: 100,
+            },
+            monster: {
+                health: 100,
+            },
+        },
+        methods: {
+            randInt: function(min, max) {
+                return Math.floor(Math.random() * (+max - +min)) + +min;
+            },
+            attack: function(min, max) {
+                this.playerAttacks(min, max);
+                this.monsterAttacks(min, max);
+            },
+            playerAttacks: function(min, max) {
+                let damage = this.randInt(min, max);
+                this.monster.health -= damage;
+                if (this.checkWin()) {
+                    return;
+                }
+                this.turns.unshift({
+                    isPlayer: true,
+                    text: `Player hits monster for ${damage}`,
+                });
+            },
+            monsterAttacks: function(min, max) {
+                let damage = this.randInt(min, max);
+                this.player.health -= damage;
+                this.checkWin();
+                this.turns.unshift({
+                    isPlayer: false,
+                    text: `Monster hits player for ${damage}`,
+                });
+            },
+            heal: function() {
+                this.monsterAttacks(0, 10);
+                let health = this.randInt(2, 10);
+                if (this.player.health + health <= 100) {
+                    this.player.health += health;
+                    this.turns.unshift({
+                        isPlayer: true,
+                        text: `Player heals for ${health}`,
+                    });
+                }
+                this.checkWin();
+            },
+            giveUp: function() {
+                this.gameRunning = false;
+            },
+            checkWin: function() {
+                if (this.monster.health <= 0) {
+                    if (confirm('You win! New Game?')) {
+                        this.newGame()
+                    } else {
+                        this.gameRunning = false;
+                    }
+                    return true;
+                } else if (this.player.health <= 0) {
+                    if (confirm('You lose! New Game?')) {
+                        this.newGame()
+                    } else {
+                        this.gameRunning = false;
+                    }
+                    return true;
+                }
+                return false;
+            },
+            newGame: function() {
+                this.gameRunning = true;
+                this.player.health = 100;
+                this.monster.health = 100;
+            },
+        },
+    });
+};
